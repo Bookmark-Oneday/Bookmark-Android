6224d2cf8ff23f59e4c819daac147e49d1aebfe5
yunsehwan <sh10302@naver.com>
2023-04-09 23:42:18 +0900

    [fix] interceptor에서 timeout 에러 수신시 실패 response 리턴하도록 수정

--- app/src/main/java/com/bookmark/bookmark_oneday/app/retrofit/NetworkInterceptor.kt
+++ app/src/main/java/com/bookmark/bookmark_oneday/app/retrofit/NetworkInterceptor.kt
@@ -1,18 +1,48 @@
 package com.bookmark.bookmark_oneday.app.retrofit
 
 import okhttp3.Interceptor
+import okhttp3.Protocol
 import okhttp3.Request
 import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
+import java.net.SocketTimeoutException
 
 class NetworkInterceptor(
     private val getToken : () -> String?
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val builder : Request.Builder = chain.request().newBuilder()
+        val request = chain.request()
+        val builder : Request.Builder = request.newBuilder()
         val jwtToken : String ?= getToken()
         if (jwtToken != null) {
-            builder.addHeader("Authorization", "Bearer $jwtToken")
+            builder.addHeader("user_id", "$jwtToken")
         }
-        return chain.proceed(builder.build())
+
+        try {
+            return chain.proceed(builder.build())
+        } catch (e: Exception) {
+            val msg: String
+            val errorCode: Int
+
+            when (e) {
+                is SocketTimeoutException -> {
+                    msg = "timeout"
+                    errorCode = 408
+                }
+                else -> {
+                    msg = "${e.message}"
+                    errorCode = -1
+                }
+            }
+
+            return Response.Builder()
+                .request(request = request)
+                .code(errorCode)
+                .message(msg)
+                .protocol(Protocol.HTTP_1_1)
+                .body("{${e}}".toResponseBody(null))
+                .build()
+        }
+
     }
 }
