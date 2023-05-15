65a990841b32b063772714c9e06b631e07a9c26a
yunsehwan <sh10302@naver.com>
2023-05-15 16:59:32 +0900

    [feature] 오늘 한줄 화면 이동시 BottomNavigation 투명화 구현

--- app/src/main/java/com/bookmark/bookmark_oneday/presentation/screens/home/HomeActivity.kt
+++ app/src/main/java/com/bookmark/bookmark_oneday/presentation/screens/home/HomeActivity.kt
@@ -2,7 +2,9 @@ package com.bookmark.bookmark_oneday.presentation.screens.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.bookmark.bookmark_oneday.R
 import com.bookmark.bookmark_oneday.databinding.ActivityHomeBinding
 import com.bookmark.bookmark_oneday.presentation.base.ViewBindingActivity
@@ -13,9 +15,39 @@ class HomeActivity : ViewBindingActivity<ActivityHomeBinding>(ActivityHomeBindin
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navhost_home_fragment) as NavHostFragment
-        val navHostController = navHostFragment.navController
+        setBottomNavigation()
     }
+
+    private fun setBottomNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navhost_home_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeNav_todayOnelineFragment -> {
+                    binding.navHomeBottom.background = ContextCompat.getDrawable(this, R.color.transparent)
+                    binding.navHomeBottom.itemTextColor = ContextCompat.getColorStateList(this, R.color.selector_menu_on_transparent)
+                    binding.navHomeBottom.itemIconTintList = ContextCompat.getColorStateList(this, R.color.selector_menu_on_transparent)
+                    window.statusBarColor = ContextCompat.getColor(this, R.color.transparent)
+                }
+                else -> {
+                    binding.navHomeBottom.background = ContextCompat.getDrawable(this, R.color.default_background)
+                    binding.navHomeBottom.itemTextColor = ContextCompat.getColorStateList(this, R.color.selector_menu_tint)
+                    binding.navHomeBottom.itemIconTintList = ContextCompat.getColorStateList(this, R.color.selector_menu_tint)
+                    window.statusBarColor = ContextCompat.getColor(this, R.color.default_background)
+
+                    if (destination.id == R.id.bookDetailFragment){
+                        binding.navHomeBottom.menu.findItem(R.id.homeNav_mylibraryFragment).isChecked = true
+                    }
+                }
+            }
+        }
+
+        binding.navHomeBottom.setupWithNavController(navController)
+    }
+
+
+
 
     /**
      * 홈 화면을 구성하는 각 fragment 에서 첫 데이터 로드 실패시 호출합니다.
---
+++ app/src/main/java/com/bookmark/bookmark_oneday/presentation/screens/home/today_oneline/TodayOnelineFragment.kt
@@ -0,0 +1,9 @@
+package com.bookmark.bookmark_oneday.presentation.screens.home.today_oneline
+
+import com.bookmark.bookmark_oneday.R
+import com.bookmark.bookmark_oneday.databinding.FragmentTodayOnelineBinding
+import com.bookmark.bookmark_oneday.presentation.base.ViewBindingFragment
+
+class TodayOnelineFragment : ViewBindingFragment<FragmentTodayOnelineBinding>(FragmentTodayOnelineBinding::bind, R.layout.fragment_today_oneline) {
+
+}
