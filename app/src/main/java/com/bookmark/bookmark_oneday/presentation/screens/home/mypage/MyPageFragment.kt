9e3358ebde40ce0b09a1d08ee1400e86f9678ac5
yunsehwan <sh10302@naver.com>
2023-06-23 15:32:13 +0900

    [feature] mypage 화면 UI 구성

---
+++ app/src/main/java/com/bookmark/bookmark_oneday/presentation/screens/home/mypage/MyPageFragment.kt
@@ -0,0 +1,37 @@
+package com.bookmark.bookmark_oneday.presentation.screens.home.mypage
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import com.bookmark.bookmark_oneday.R
+import com.bookmark.bookmark_oneday.databinding.FragmentMypageBinding
+import com.bookmark.bookmark_oneday.presentation.base.ViewBindingFragment
+
+class MyPageFragment : ViewBindingFragment<FragmentMypageBinding>(
+    FragmentMypageBinding::bind,
+    R.layout.fragment_mypage
+) {
+    private val viewModel : MyPageViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setButton()
+    }
+
+    private fun setButton() {
+        binding.partialMypageProfile.btnMypageProfileSetting.setOnClickListener {
+
+        }
+
+        binding.llbtnMypageAlarmSetting.setOnClickListener {
+
+        }
+
+        binding.llbtnMypageDataClear.setOnClickListener {
+
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/bookmark/bookmark_oneday/presentation/screens/home/mypage/MyPageViewModel.kt
@@ -0,0 +1,12 @@
+package com.bookmark.bookmark_oneday.presentation.screens.home.mypage
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyPageViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
