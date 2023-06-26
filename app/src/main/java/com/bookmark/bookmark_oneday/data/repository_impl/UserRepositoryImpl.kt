039d8cc4c54c2ec26328c9de1f350f34de6f040a
yunsehwan <sh10302@naver.com>
2023-06-26 15:19:55 +0900

    [feature] 사용자 정보 관련 useCase, Repository 구현

---
+++ app/src/main/java/com/bookmark/bookmark_oneday/data/repository_impl/UserRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.bookmark.bookmark_oneday.data.repository_impl
+
+import com.bookmark.bookmark_oneday.data.datasource.user_datasource.UserDataSource
+import com.bookmark.bookmark_oneday.domain.model.UserInfo
+import com.bookmark.bookmark_oneday.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val userDataSource: UserDataSource
+) : UserRepository {
+    override fun getUserProfile(): Flow<UserInfo> = userDataSource.getUser().map { userEntity ->
+        UserInfo(nickname = userEntity.nickname, profileImage = userEntity.profileUri, bio = userEntity.bio)
+    }
+
+    override suspend fun setUserProfile(nickname: String, profileImage: String?, bio: String) {
+        userDataSource.setUser(nickname, profileImage, bio)
+    }
+
+    override suspend fun setReadingTime(readingTime: Int) {
+        userDataSource.setReadTime(readingTime)
+    }
+
+}
---
+++ app/src/main/java/com/bookmark/bookmark_oneday/domain/model/UserInfo.kt
@@ -0,0 +1,11 @@
+package com.bookmark.bookmark_oneday.domain.model
+
+data class UserInfo(
+    val nickname : String,
+    val profileImage : String? = null,
+    val bio : String
+) {
+    companion object {
+        val defaultUserInfo = UserInfo(nickname = "loading...", profileImage = null, bio = "로딩중입니다.")
+    }
+}
