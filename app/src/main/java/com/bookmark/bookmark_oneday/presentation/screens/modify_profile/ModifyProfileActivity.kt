1c3d2b91b3e37fa4b7703535134941eb5d0fd9c7
yunsehwan <sh10302@naver.com>
2023-06-27 10:20:48 +0900

    [fix] 프로필 수정 화면에서 한줄소개, 이미지 변경 추가

--- app/src/main/java/com/bookmark/bookmark_oneday/presentation/screens/modify_profile/ModifyProfileActivity.kt
+++ app/src/main/java/com/bookmark/bookmark_oneday/presentation/screens/modify_profile/ModifyProfileActivity.kt
@@ -1,34 +1,80 @@
 package com.bookmark.bookmark_oneday.presentation.screens.modify_profile
 
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import com.bookmark.bookmark_oneday.R
 import com.bookmark.bookmark_oneday.databinding.ActivityModifyProfileBinding
 import com.bookmark.bookmark_oneday.presentation.base.ViewBindingActivity
+import com.bookmark.bookmark_oneday.presentation.util.collectLatestInLifecycle
+import com.bumptech.glide.Glide
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ModifyProfileActivity : ViewBindingActivity<ActivityModifyProfileBinding>(ActivityModifyProfileBinding::inflate) {
     private val viewModel : ModifyProfileViewModel by viewModels()
-
+    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        uri?.let { viewModel.setProfileImageUri(uri.toString()) }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setButton()
+        setObserver()
+        setEditText()
     }
 
     private fun setButton() {
         binding.btnModifyProfileComplete.setOnClickListener {
-
+            viewModel.tryModifyUserInfo()
         }
 
         binding.btnModifyProfileAddImage.setOnClickListener {
-
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
 
         binding.btnModifyProfileRemoveImage.setOnClickListener {
-
+            viewModel.setProfileImageUri(null)
         }
 
         binding.btnModifyProfileBack.setOnClickListener {
             finish()
         }
     }
+
+    private fun setObserver() {
+        viewModel.modifyProfileResult.collectLatestInLifecycle(owner = this) { success ->
+            if (success) finish()
+        }
+
+        viewModel.bio.collectLatestInLifecycle(this) {
+            binding.edittextModifyProfile.setText(it)
+        }
+
+        viewModel.nickname.collectLatestInLifecycle(this) {
+            binding.labelModifyProfileNickname.text = it
+        }
+
+        viewModel.profileImageUri.collectLatestInLifecycle(this) { imageUri ->
+            if (imageUri == null) {
+                binding.btnModifyProfileAddImage.visibility = View.VISIBLE
+                binding.btnModifyProfileRemoveImage.visibility = View.INVISIBLE
+                binding.imgSignupProfile.setBackgroundResource(R.drawable.ic_all_default_profile)
+            } else {
+                binding.btnModifyProfileAddImage.visibility = View.INVISIBLE
+                binding.btnModifyProfileRemoveImage.visibility = View.VISIBLE
+                Glide.with(baseContext).load(imageUri).into(binding.imgSignupProfile)
+            }
+        }
+
+    }
+
+    private fun setEditText(){
+        binding.edittextModifyProfile.setTextChangedListener {
+            viewModel.setBio(it)
+        }
+    }
+
 }
--- app/src/main/java/com/bookmark/bookmark_oneday/presentation/screens/modify_profile/ModifyProfileViewModel.kt
+++ app/src/main/java/com/bookmark/bookmark_oneday/presentation/screens/modify_profile/ModifyProfileViewModel.kt
@@ -1,12 +1,57 @@
 package com.bookmark.bookmark_oneday.presentation.screens.modify_profile
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bookmark.bookmark_oneday.domain.usecase.UseCaseGetUser
+import com.bookmark.bookmark_oneday.domain.usecase.UseCaseSetUser
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ModifyProfileViewModel @Inject constructor(
-
+    useCaseGetUser: UseCaseGetUser,
+    private val useCaseSetUser: UseCaseSetUser
 ): ViewModel() {
+    private val userInfo = useCaseGetUser.getProfile()
 
+    private val _bio = MutableStateFlow("")
+    private val _nickname = MutableStateFlow("")
+    private val _profileImageUri = MutableStateFlow<String?>(null)
+
+    val bio = _bio.asStateFlow()
+    val nickname = _nickname.asStateFlow()
+    val profileImageUri = _profileImageUri.asStateFlow()
+
+    private val _modifyProfileResult = MutableSharedFlow<Boolean>()
+    val modifyProfileResult = _modifyProfileResult.asSharedFlow()
+
+    init {
+        viewModelScope.launch {
+            userInfo.collectLatest {
+                _bio.value = it.bio
+                _nickname.value = it.nickname
+                _profileImageUri.value = it.profileImage
+            }
+        }
+    }
+    fun setProfileImageUri(imageUri : String?) {
+        _profileImageUri.value = imageUri
+    }
+
+    fun setBio(inputBio : String) {
+        _bio.value = inputBio
+    }
+
+    fun tryModifyUserInfo() {
+        viewModelScope.launch {
+            useCaseSetUser.setUserProfile(nickname = _nickname.value, bio = _bio.value, profileUri = _profileImageUri.value)
+            _modifyProfileResult.emit(true)
+        }
+    }
 }
