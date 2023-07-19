95f361796114036659a05acaf48e42632c158a34
yunsehwan <sh10302@naver.com>
2023-07-19 16:09:39 +0900

    [feature] 달력 화면 뼈대 생성 및 homeNavigation 에 추가

---
+++ app/src/main/java/com/bookmark/bookmark_oneday/presentation/screens/home/reading_calendar/ReadingCalendarFragment.kt
@@ -0,0 +1,16 @@
+package com.bookmark.bookmark_oneday.presentation.screens.home.reading_calendar
+
+import android.os.Bundle
+import android.view.View
+import com.bookmark.bookmark_oneday.R
+import com.bookmark.bookmark_oneday.databinding.FragmentReadingCalendarBinding
+import com.bookmark.bookmark_oneday.presentation.base.ViewBindingFragment
+
+class ReadingCalendarFragment : ViewBindingFragment<FragmentReadingCalendarBinding>(FragmentReadingCalendarBinding::bind, R.layout.fragment_reading_calendar) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
