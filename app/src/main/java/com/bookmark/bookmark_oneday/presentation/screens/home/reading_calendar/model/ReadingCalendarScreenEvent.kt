9b197b1ef5891d3d05bef8241e13f055923f6eb2
yunsehwan <sh10302@naver.com>
2023-07-20 02:04:51 +0900

    [fix] 월 변경시 날짜 선택 유도 UI가 다시 표시되도록 변경

--- app/src/main/java/com/bookmark/bookmark_oneday/presentation/screens/home/reading_calendar/model/ReadingCalendarScreenEvent.kt
+++ app/src/main/java/com/bookmark/bookmark_oneday/presentation/screens/home/reading_calendar/model/ReadingCalendarScreenEvent.kt
@@ -22,6 +22,7 @@ sealed class ReadingCalendarScreenEvent {
         override fun reduce(prevState: ReadingCalendarScreenState): ReadingCalendarScreenState {
             return prevState.copy(
                 showCalendarCellLoading = false,
+                showReadingHistoryLoadingIntro = true,
                 readingHistoryCalendar = ReadingHistoryCalendar(
                     year = year,
                     month = month,
