3aa99e4ddf99cb259b097b804486769df6b8d789
yunsehwan <sh10302@naver.com>
2023-09-08 00:08:14 +0900

    [setting] SCHEDULE_EXACT_ALARM 검사 및 요청 부분 추가

--- app/src/main/java/com/bookmark/bookmark_oneday/presentation/util/PermissionUtils.kt
+++ app/src/main/java/com/bookmark/bookmark_oneday/presentation/util/PermissionUtils.kt
@@ -1,8 +1,10 @@
 package com.bookmark.bookmark_oneday.presentation.util
 
 import android.Manifest
+import android.app.AlarmManager
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.content.ContextCompat
 
 val CAMERA_REQUIRED_PERMISSIONS =
@@ -14,4 +16,21 @@ internal fun Context.checkCameraPermissionGranted(
     permissionList : Array<String>
 ) = permissionList.all {
     ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+}
+
+val EXACT_ALARM_PERMISSION =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        Manifest.permission.SCHEDULE_EXACT_ALARM
+    } else {
+        null
+    }
+
+
+fun checkExactAlarmAvailable(context : Context) : Boolean {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.canScheduleExactAlarms()
+    } else {
+        true
+    }
 }
